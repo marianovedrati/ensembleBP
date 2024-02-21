96599fb13d15bf07d3c1d503c81df04133dfc25e
Giorgio Montesi <Giorgio@MBPdiGiorgio2.homenet.telecomitalia.it>
2024-02-21 12:13:44 +0100

    svm and voom working

--- Codes/benchMLSeq.R
+++ Codes/benchMLSeq.R
@@ -81,17 +81,17 @@ trainTest.split <- function(df, class, ratio = 0.3){
 svm.based <- function(data.trainS4, data.testS4, classts, 
                       tL = 2, n = 5, r = 2){
   
+  # Define control function for all svm.based classifiers
   svmControl <- trainControl(method = "repeatedcv", number = n,
                              repeats = r, classProbs = TRUE)
-  set.seed(2128)
+  
+  print("Fitting SVM-Radial")
+  set.seed(1510)
   # Support vector machines with radial basis function kernel
   fit.svmRadial <- classify(data = data.trainS4, method = "svmRadial",
                             preProcessing = "deseq-vst", ref = "D", tuneLength = tL,
                             control = svmControl)
 
-  # show(fit.svm)
-  # trained(fit.svm)
-  # plot(fit.svm)
   #Predicted class labels
   pred.svmRadial <- predict(fit.svmRadial, data.testS4)
   pred.svmRadial <- relevel(pred.svmRadial, ref = "D")
@@ -100,8 +100,105 @@ svm.based <- function(data.trainS4, data.testS4, classts,
   tblRadial <- table(Predicted = pred.svmRadial, Actual = actual)
   svmRadial.cm <- confusionMatrix(tblRadial, positive = "D")
   
-  return(list(svmRadial.cm))
+  print("Fitting SVM-Poly")
+  set.seed(1510)
+  # Support vector machines with poly basis function kernel
+  fit.svmPoly <- classify(data = data.trainS4, method = "svmPoly",
+                          preProcessing = "deseq-vst", ref = "D", tuneLength = tL,
+                          control = svmControl)
+  
+  print(selectedGenes(fit.svmPoly))
+  #Predicted class labels
+  pred.svmPoly <- predict(fit.svmPoly, data.testS4)
+  pred.svmPoly <- relevel(pred.svmPoly, ref = "D")
+  
+  tblPoly <- table(Predicted = pred.svmPoly, Actual = actual)
+  svmPoly.cm <- confusionMatrix(tblPoly, positive = "D")
+  
+  print("Fitting SVM-Linear")
+  set.seed(1510)
+  # Support vector machines with linear basis function kernel
+  fit.svmLinear <- classify(data = data.trainS4, method = "svmLinear",
+                            preProcessing = "deseq-vst", ref = "D", tuneLength = tL,
+                            control = svmControl)
+  
+  #Predicted class labels
+  pred.svmLinear <- predict(fit.svmLinear, data.testS4)
+  pred.svmLinear <- relevel(pred.svmLinear, ref = "D")
+  
+  tblLinear <- table(Predicted = pred.svmLinear, Actual = actual)
+  svmLinear.cm <- confusionMatrix(tblLinear, positive = "D")
+  
+  print("Successfully accomplished SVM-based methods")
+  
+  return(list(svmRadial.cm, svmPoly.cm, svmLinear.cm))
 
+}
+
+
+
+#' @description Tests several VOOM-based classifiers
+#' @param data.trainS4
+#' @param data.testS4
+#' @param classts
+#' @param tL tune Length
+#' @param n number of CV
+#' @param r number of repeats for CV
+#' @returns A list of Confusion Matrices one for each VOOM-based method
+voom.based <- function(data.trainS4, data.testS4, classts, 
+                       tL = 2, n = 5, r = 2){
+  
+  # Define control function for all voom.based classifiers
+  voomControl <- voomControl(method = "repeatedcv", number = n, repeats = r,
+                             tuneLength = tL)
+  
+  print("Fitting voom-DLDA")
+  set.seed(1510)
+  # voomDLDA
+  fit.voomDLDA <- classify(data = data.trainS4, method = "voomDLDA",
+                            normalize = "deseq", ref = "D",
+                            control = voomControl)
+  
+  #Predicted class labels
+  pred.voomDLDA <- predict(fit.voomDLDA, data.testS4)
+  pred.voomDLDA <- relevel(pred.voomDLDA, ref = "D")
+  actual <- relevel(classts$condition, ref = "D")
+  
+  tblDLDA <- table(Predicted = pred.voomDLDA, Actual = actual)
+  voomDLDA.cm <- confusionMatrix(tblDLDA, positive = "D")
+  
+  print("Fitting voom-DQDA")
+  set.seed(1510)
+  # voomDQDA
+  fit.voomDQDA <- classify(data = data.trainS4, method = "voomDQDA",
+                           normalize = "deseq", ref = "D",
+                           control = voomControl)
+  
+  #Predicted class labels
+  pred.voomDQDA <- predict(fit.voomDQDA, data.testS4)
+  pred.voomDQDA <- relevel(pred.voomDQDA, ref = "D")
+  
+  tblDQDA <- table(Predicted = pred.voomDQDA, Actual = actual)
+  voomDQDA.cm <- confusionMatrix(tblDQDA, positive = "D")
+  
+  print("Fitting voom-NSC")
+  set.seed(1510)
+  # voomNSC
+  fit.voomNSC <- classify(data = data.trainS4, method = "voomNSC",
+                           normalize = "deseq", ref = "D",
+                           control = voomControl)
+  
+  #Predicted class labels
+  pred.voomNSC <- predict(fit.voomNSC, data.testS4)
+  pred.voomNSC <- relevel(pred.voomNSC, ref = "D")
+  
+  tblNSC <- table(Predicted = pred.voomNSC, Actual = actual)
+  voomNSC.cm <- confusionMatrix(tblNSC, positive = "D")
+  
+  print("Successfully accomplished VOOM-based methods")
+  
+  return(list(voomDLDA.cm, voomDQDA.cm, voomNSC.cm))
+  
 }
 
 
@@ -117,7 +214,7 @@ data.testS4 <- tts[[2]]
 classts <- tts[[3]]
 
 svm <- svm.based(data.trainS4, data.testS4, classts)
-
+voom <- voom.based(data.trainS4, data.testS4, classts)
 
 
 
