6ae0711cad1913ce661b30508e168bd7d54d0f9b
Giorgio Montesi <Giorgio@MacBook-Pro-di-Giorgio-2.local>
2024-03-22 15:36:23 +0100

    Added TODO in funzione_string.R

--- Codes/funzione_String.R
+++ Codes/funzione_String.R
@@ -9,54 +9,56 @@
 ## TODO: verificare che il nome dei geni sia nel formato necessario per STRING, 
 ##       altrimenti cambiare i nomi!!
 analyze_gene_network <- function(gene_list, algorithm = "fastgreedy") {
+  
   library(STRINGdb)
   library(ggplot2)
   
-  # Trasformo la lista di geni in un dataframe
+  # Change type of gene_list from list to df
   gene_data <- as.data.frame(gene_list)
   
-  # Imposta i nomi delle colonne
+  # Set colnames properly
   colnames(gene_data) <- "Gene"
   
-  # Inizializza il database STRING
+  # Initialize STRING database
   ## TODO: questi parametri devono essere tutti richiesti in input dalla funzione
+  ##       è comunque da considerare che non tutti lavorano con gli Umani
   string_db <- STRINGdb$new(version = "12", species = 9606, network_type = "full", input_directory = "")
   
-  # Mappa i geni al database STRING e rimuovi quelli non mappati
+  # Map genes to STRING database and remove unmapped ones
   mapped_genes <- string_db$map(gene_data, "Gene", removeUnmappedRows = TRUE)
   
-  # Plot della rete STRING con link e summary
-  # Salvataggio del grafico della rete STRING 
+  # Plot and save STRING Network with link and summary
+  ## TODO: il path del salvataggio deve essere chiesto da input 
   plot_network_general_path <- file.path(getwd(), "../Results/plot_network_general.pdf")
   pdf(plot_network_general_path)
   string_db$plot_network(mapped_genes, add_link = TRUE, add_summary = TRUE)
   dev.off()
   
-  # Calcola l'arricchimento nelle annotazioni GO
+  # Compute GO enrichment of general ppi Network
   enrichmentGO <- string_db$get_enrichment(mapped_genes, category = "Process", iea = TRUE)
-  # Calcola l'arricchimento nelle annotazioni KEGG
+  # Compute KEGG enrichment of general ppi Network
   enrichmentKEGG <- string_db$get_enrichment(mapped_genes, category = "KEGG", iea = TRUE)
   
-  # Ottieni i clusters
+  # Compute Clusters
+  ## TODO: questo step va fatto solo se richiesto da input
   clustersList <- string_db$get_clusters(mapped_genes$STRING_id, algorithm = algorithm)
-  ## TODO: farsi dare il parametro 3 da input
+  ## TODO: farsi dare il parametro 5 da input
   clustersList <- clustersList[lengths(clustersList) > 5]
   
-  # Imposta il percorso del file PDF per i plot dei cluster
+  # Set path to save PDF plots of clusters
+  ## TODO: il path del salvataggio va chiesto da input
   clusters_plot_path <- file.path(getwd(), "../Results/clusters_plot.pdf")
   
-  # Inizializza il dispositivo grafico PDF per i plot dei cluster
+  # Initialize pdf plot of ppi clusters
   pdf(clusters_plot_path)
   
-  # Crea i plot per tutti i clusters
+  # Build one plot for each cluster
   for (i in 1:length(clustersList)) {
     string_db$plot_network(clustersList[[i]])
   }
-  
-  # Chiudi il dispositivo grafico PDF per i plot dei cluster
   dev.off()
   
-  # Restituzione dei risultati
+  # Build list containing all results
   result <- list(
     plot_network_general = plot_network_general_path,
     enrichmentGO = enrichmentGO,
@@ -68,17 +70,15 @@ analyze_gene_network <- function(gene_list, algorithm = "fastgreedy") {
 }
 
 
-# # Utilizzo della funzione
-list <- readRDS("../Results/list_genes_2.rds")
-gene_list <- list[[4]]
-#
-# # Applico funzione
-result <-  analyze_gene_network(gene_list)
+# ## Utilizzo della funzione
+# list <- readRDS("../Results/list_genes_2.rds")
+# gene_list <- list[[4]]
+# #
+# ## Applico funzione
+# result <-  analyze_gene_network(gene_list)
 
 
 
-result$enrichmentKEGG$description
-result$enrichmentGO$description
 
 
 
